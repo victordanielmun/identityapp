6ed9595b9e101085f17eb2668f6887a0e6166797
victordanielmun <victordanielmun@gmail.com>
2024-06-25 14:13:29 -0500

    Add nuget and data migration Identity user

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,8 @@
+using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -10,6 +14,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//se define el servicios de autenticacion
+builder.Services.AddIdentityCore<User>(options =>
+{
+    // password options
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    // user options
+    options.User.RequireUniqueEmail = true;
+    // email options
+    options.SignIn.RequireConfirmedEmail = true;
+});
+
 
 var app = builder.Build();
 
